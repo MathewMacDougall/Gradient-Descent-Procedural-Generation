f648aef02c2117c7b47a65ae22147defe4d04178
Mathew MacDougall <mmacd97@gmail.com>
2021-01-09 19:39:52 -0800

    set up basic structure for saving generation data to a file

--- gd_procgen/main.py
+++ gd_procgen/main.py
@@ -1,5 +1,27 @@
-def main():
-    pass
+import numpy as np
+from gd_procgen.gradient_descent import minimize, GRAD_TYPE
+from pathlib import Path
+import argparse
+
+def main(output_file):
+    x0 = np.array([2, 5])
+    def cost_func(x):
+        return np.linalg.norm(x, 2)
+    data = []
+    def callback(x):
+        data.append(x)
+    minimize(cost_func, x0, grad_type=GRAD_TYPE.BATCH, callback=callback)
+    np.savetxt(str(output_file), np.asarray(data), delimiter=",")
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_file", type=Path)
+
+    args = parser.parse_args()
+
+    output_file = args.output_file
+    output_file = output_file.resolve()
+    assert output_file.suffix == ".csv"
+
+    main(output_file)
