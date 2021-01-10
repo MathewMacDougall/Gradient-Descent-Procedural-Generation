06378dae1af0c35f30aeea754f97b386e0fe07e9
Mathew MacDougall <mmacd97@gmail.com>
2021-01-09 20:49:18 -0800

    cheeky closures make good interfaces

--- gd_procgen/main.py
+++ gd_procgen/main.py
@@ -1,37 +1,45 @@
 import numpy as np
 from gd_procgen.gradient_descent import minimize, GRAD_TYPE
-from pathlib import Path
-import argparse
-from gd_procgen.animation import play_animation
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+
 class OptimizationProblem:
-    def __init__(self, x0, cost_func):
+    def __init__(self, x0, cost_func, draw_func):
         self._data = []
         self._x0 = x0
         self._cost_func = cost_func
+        self._animation_func = None
+        fig, ax = plt.subplots()
+        self._fig = fig
+        self._ax = ax
+        self._set_animation_function(draw_func)
 
     def optimize(self):
         def callback(x):
             self._data.append(x)
 
-        minimize(self._cost_func, self._x0, grad_type=GRAD_TYPE.BATCH, callback=callback)
+        minimize(
+            self._cost_func, self._x0, grad_type=GRAD_TYPE.BATCH, callback=callback
+        )
+
+    def _set_animation_function(self, func):
+        def animate(i):
+            data = self._data[i]
+            return func(data,self._ax)
+
+        self._animation_func = animate
+
 
     def play_animation(self):
-        fig, ax = plt.subplots()
-        ax.set_xlim(-5, 5)
-        ax.set_ylim(-5, 5)
-        ax.set_aspect("equal")
-        ax.grid()
-
-        def animate(i):
-            x = self._data[i][0]
-            y = self._data[i][1]
-            return ax.plot(x, y, "ro")
+        # TODO: set bounds from data
+        self._ax.set_xlim(-5, 5)
+        self._ax.set_ylim(-5, 5)
+        self._ax.set_aspect("equal")
+        self._ax.grid()
 
         ani = animation.FuncAnimation(
-            fig, animate, len(self._data), interval=0.1 * 1000, blit=True
+            self._fig, self._animation_func, len(self._data), interval=0.1 * 1000, blit=True
         )
         plt.show()
 
@@ -42,7 +50,12 @@ def main():
     def cost_func(x):
         return np.linalg.norm(x, 2)
 
-    problem = OptimizationProblem(x0, cost_func)
+    def draw(data, ax):
+        x = data[0]
+        y = data[1]
+        return ax.plot(x, y, "ro")
+
+    problem = OptimizationProblem(x0, cost_func, draw)
     problem.optimize()
     problem.play_animation()
 
