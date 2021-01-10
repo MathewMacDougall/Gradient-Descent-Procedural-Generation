31700ade671b75c27c8fb74a10ccd9bb28974902
Mathew MacDougall <mmacd97@gmail.com>
2021-01-09 20:34:52 -0800

    seeing if the animation is easier to set up with a class

--- gd_procgen/animation.py
+++ gd_procgen/animation.py
@@ -2,21 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+
 def play_animation(filepath, draw_function):
     assert filepath.is_file()
-    data =  np.genfromtxt(str(filepath), delimiter=",")
+    data = np.genfromtxt(str(filepath), delimiter=",")
 
     fig, ax = plt.subplots()
     ax.set_xlim(-5, 5)
     ax.set_ylim(-5, 5)
-    ax.set_aspect('equal')
+    ax.set_aspect("equal")
     ax.grid()
 
-    def animate(i):
-        x = data[i][0]
-        y = data[i][1]
-        return ax.plot(x, y, 'ro')
-
     ani = animation.FuncAnimation(
-        fig, animate, len(data), interval=0.1*1000, blit=True)
+        fig, draw_function, len(data), interval=0.1 * 1000, blit=True
+    )
     plt.show()
--- gd_procgen/main.py
+++ gd_procgen/main.py
@@ -3,34 +3,49 @@ from gd_procgen.gradient_descent import minimize, GRAD_TYPE
 from pathlib import Path
 import argparse
 from gd_procgen.animation import play_animation
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+class OptimizationProblem:
+    def __init__(self, x0, cost_func):
+        self._data = []
+        self._x0 = x0
+        self._cost_func = cost_func
+
+    def optimize(self):
+        def callback(x):
+            self._data.append(x)
+
+        minimize(self._cost_func, self._x0, grad_type=GRAD_TYPE.BATCH, callback=callback)
+
+    def play_animation(self):
+        fig, ax = plt.subplots()
+        ax.set_xlim(-5, 5)
+        ax.set_ylim(-5, 5)
+        ax.set_aspect("equal")
+        ax.grid()
+
+        def animate(i):
+            x = self._data[i][0]
+            y = self._data[i][1]
+            return ax.plot(x, y, "ro")
+
+        ani = animation.FuncAnimation(
+            fig, animate, len(self._data), interval=0.1 * 1000, blit=True
+        )
+        plt.show()
 
 
-def main(output_file):
+def main():
     x0 = np.array([2, 5])
 
     def cost_func(x):
         return np.linalg.norm(x, 2)
 
-    data = []
-
-    def callback(x):
-        data.append(x)
-
-    minimize(cost_func, x0, grad_type=GRAD_TYPE.BATCH, callback=callback)
-    np.savetxt(str(output_file), np.asarray(data), delimiter=",")
-
-    play_animation(output_file, None)
-
+    problem = OptimizationProblem(x0, cost_func)
+    problem.optimize()
+    problem.play_animation()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--output_file", type=Path)
-
-    args = parser.parse_args()
-
-    output_file = args.output_file
-    output_file = output_file.resolve()
-    assert output_file.suffix == ".csv"
-
-    main(output_file)
+    main()
