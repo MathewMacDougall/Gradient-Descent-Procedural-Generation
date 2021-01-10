completely redid the gradient functions and fixed many bugs

gd_procgen/gradient_descent.py
@@ -1,6 +1,13 @@
 import numpy as np
 import copy
 import random
+import itertools
+from enum import Enum
+
+class GRAD_TYPE(Enum):
+    BATCH = 0
+    SEQUENTIAL_STOCHASTIC = 1
+    RANDOM_STOCHASTIC = 2
 
 def _batch_grad(cost_fun, x: np.array):
     ret = []
@@ -15,52 +22,77 @@ def _batch_grad(cost_fun, x: np.array):
         ret.append(g)
     return np.array(ret)
 
-def _sequential_stochastic_grad(cost_fun, x: np.array):
-    def _sequential_stochastic_grad_helper(cost_fun, x: np.array):
-        dx = 1e-3
-        index = 0
-        while True:
+def _create_sequential_stochastic_grad_func():
+    class SSGrad:
+        def __init__(self):
+            self.indices = itertools.count(0)
+
+        def grad(self, cost_fun, x):
+            dx = 1e-3
+            index = next(self.indices) % x.size
             xpos = x.copy()
             xpos[index] += dx
             xneg = x.copy()
             xneg[index] -= dx
             diff = cost_fun(xpos) - cost_fun(xneg)
             g = diff / (2 * dx)
+            ret = np.zeros(x.size)
+            ret[index] = g
 
-            index += 1 % len(x)
+            return np.asarray(ret)
 
-            yield np.asarray(g)
+    ss_grad = SSGrad()
+    def grad(cost_fun, x):
+        return ss_grad.grad(cost_fun, x)
 
-    return next(_sequential_stochastic_grad_helper(cost_fun, x))
+    return grad
 
-def _random_stochastic_grad(cost_fun, x: np.array):
-    dx = 1e-3
-    index = random.randint(0, x.size-1)
-    xpos = x.copy()
-    xpos[index] += dx
-    xneg = x.copy()
-    xneg[index] -= dx
-    diff = cost_fun(xpos) - cost_fun(xneg)
-    g = diff / (2 * dx)
-    return np.asarray(g)
 
-GRAD_TYPE = {
-    "batch": _batch_grad,
-    "seq_stoc": _sequential_stochastic_grad,
-    "rand_stoc": _random_stochastic_grad
-}
+def _create_random_stochastic_grad_func(rand_seed=None):
+    class RSGrad:
+        def __init__(self, rand_seed):
+            self.rng = np.random.RandomState(rand_seed)
 
-def maximize(cost_func, x0, grad_type="batch", max_iters=500, rand_seed=None):
+        def grad(self, cost_fun, x):
+            dx = 1e-3
+            index = self.rng.randint(0, x.size)
+            xpos = x.copy()
+            xpos[index] += dx
+            xneg = x.copy()
+            xneg[index] -= dx
+            diff = cost_fun(xpos) - cost_fun(xneg)
+            g = diff / (2 * dx)
+            ret = np.zeros(x.size)
+            ret[index] = g
+            return np.asarray(ret)
+
+    rs_grad = RSGrad(rand_seed)
+    def grad(cost_fun, x):
+        return rs_grad.grad(cost_fun, x)
+
+    return grad
+
+
+def _create_grad_func(grad_type, rand_seed=None):
+    if grad_type == GRAD_TYPE.BATCH:
+        return _batch_grad
+    elif grad_type == GRAD_TYPE.SEQUENTIAL_STOCHASTIC:
+        return _create_sequential_stochastic_grad_func()
+    elif grad_type == GRAD_TYPE.RANDOM_STOCHASTIC:
+        return _create_random_stochastic_grad_func(rand_seed)
+    else:
+        raise ValueError("Invalid gradient type")
+
+
+def maximize(cost_func, x0, grad_type=GRAD_TYPE.BATCH, max_iters=500, rand_seed=None):
     new_cost_func = lambda x: -cost_func(x)
     return minimize(new_cost_func, x0, grad_type, max_iters, rand_seed)
 
-def minimize(cost_func, x0, grad_type="batch", max_iters=500, rand_seed=None):
-    random.seed(rand_seed)
-
+def minimize(cost_func, x0, grad_type=GRAD_TYPE.BATCH, max_iters=500, rand_seed=None):
     if not isinstance(cost_func(x0), (int, float, np.float64, np.int64)):
         raise RuntimeError("Cost func outputs uneexpected type {}".format(type(cost_func(x0))))
 
-    _grad = GRAD_TYPE.get(grad_type, None)
+    _grad = _create_grad_func(grad_type, rand_seed)
     assert _grad
 
     x0 = x0.astype(np.float64)
@@ -70,7 +102,8 @@ def minimize(cost_func, x0, grad_type="batch", max_iters=500, rand_seed=None):
     while current_iteration < max_iters:
         current_iteration += 1
         grad = _grad(cost_func, x)
-        x = x - nabla*grad
+        step = -nabla*grad
+        x = x + step
 
     return x, cost_func(x)
 

test/gradient_descent_test.py
@@ -6,32 +6,43 @@ import pytest
 from pytest import approx
 from gd_procgen.gradient_descent import GRAD_TYPE, minimize, maximize
 import numpy as np
+import itertools
 
 class TestGradientDescent:
-    @pytest.mark.parametrize("grad_type", list(GRAD_TYPE.keys()))
+    RAND_SEED = 42
+
+    @pytest.mark.parametrize("grad_type", list(GRAD_TYPE))
     def test_minimize_single_value_function(self, grad_type):
         # f = x^2
         def cost_func(x):
             return x[0]**2
         x0 = np.array([5])
-        x, cost = minimize(cost_func, x0, max_iters=500, grad_type=grad_type)
+        x, cost = minimize(cost_func, x0, max_iters=500, grad_type=grad_type, rand_seed=TestGradientDescent.RAND_SEED)
         assert x == approx(np.array([0]), abs=1e-3)
 
-    @pytest.mark.parametrize("grad_type", list(GRAD_TYPE.keys()))
+    @pytest.mark.parametrize("grad_type", list(GRAD_TYPE))
     def test_maximize_single_value_function(self, grad_type):
         # f = -x^2
         def cost_func(x):
-            return -x[0]**2
+            return -(x[0]**2)
         x0 = np.array([-3.4])
-        x, cost = maximize(cost_func, x0, max_iters=500, grad_type=grad_type)
+        x, cost = maximize(cost_func, x0, max_iters=500, grad_type=grad_type, rand_seed=TestGradientDescent.RAND_SEED)
         assert x == approx(np.array([0]), abs=1e-3)
 
-    # @parameterized.expand(GRAD_TYPE.keys())
-    # def test_minimize_single_value_function(self, grad_type):
-    #     # f = x^2 + y^2 + 20
-    #     def cost_func(x):
-    #         return x[0]**2
-    #     x0 = np.array([5, -6.5])
-    #     x, cost = minimize(cost_func, x0, max_iters=500, grad_type=grad_type)
-    #     assert x == approx(np.array([0, 0]), abs=1e-3)
-    #     # TODO: you are here. Need to add more complicated tests for gradient descent
+    @pytest.mark.parametrize("grad_type", list(GRAD_TYPE))
+    def test_minimize_multi_valued_function(self, grad_type):
+        # f = x^2 + y^2 + 20
+        def cost_func(x):
+            return x[0]**2 + x[1]**2 + 20
+        x0 = np.array([5, -6.5])
+        x, cost = minimize(cost_func, x0, max_iters=500, grad_type=grad_type, rand_seed=TestGradientDescent.RAND_SEED)
+        assert x == approx(np.array([0, 0]), abs=1e-3)
+
+    @pytest.mark.parametrize("grad_type", list(GRAD_TYPE))
+    def test_minimize_multi_valued_function_with_offsets(self, grad_type):
+        # f = (x+5)^2 + (y-4)^2 + 20
+        def cost_func(x):
+            return (x[0] + 5) ** 2 + (x[1] - 4) ** 2 + 20
+        x0 = np.array([0, 0])
+        x, cost = minimize(cost_func, x0, max_iters=500, grad_type=grad_type, rand_seed=TestGradientDescent.RAND_SEED)
+        assert x == approx(np.array([-5, 4]), abs=1e-3)