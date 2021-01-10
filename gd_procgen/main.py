a8d0737b755b8ef8d5c103c0dd9052b025f26d08
Mathew MacDougall <mmacd97@gmail.com>
2021-01-09 19:57:37 -0800

    Added super basic structure for playing animations of optimization

--- gd_procgen/main.py
+++ gd_procgen/main.py
@@ -2,16 +2,25 @@ import numpy as np
 from gd_procgen.gradient_descent import minimize, GRAD_TYPE
 from pathlib import Path
 import argparse
+from gd_procgen.animation import play_animation
+
 
 def main(output_file):
     x0 = np.array([2, 5])
+
     def cost_func(x):
         return np.linalg.norm(x, 2)
+
     data = []
+
     def callback(x):
         data.append(x)
+
     minimize(cost_func, x0, grad_type=GRAD_TYPE.BATCH, callback=callback)
     np.savetxt(str(output_file), np.asarray(data), delimiter=",")
+
+    play_animation(output_file, None)
+
 
 
 if __name__ == "__main__":
